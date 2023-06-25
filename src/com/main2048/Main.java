591962442a80ad4e06aefe77c80193101b9ab7c4
XBACHS\XBachs <bach.tx215528@sis.hust.edu.vn>
2023-06-25 21:07:55 +0700

    Gõ lại class main như trên clip 1

--- src/com/main2048/Main.java
+++ src/com/main2048/Main.java
@@ -1,31 +1,107 @@
 package com.main2048;
 
 import java.awt.*;
+import java.awt.image.*;
+import javax.swing.JFrame;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import com.main2048.game.Game;
+
 public class Main extends Canvas implements Runnable {
-
+	
+	public static final int WIDTH = 400,HEIGHT = 400;
+	public static float scale = 2.0f;
+	
+	public JFrame frame;
+	public Thread thread;
+	public Game game;
+	public boolean running = false;
+	
+	public static BufferedImage image = new BufferedImage( WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB); 
+	public static int[] pixels = (( DataBufferInt) image.getRaster().getDataBuffer()).getData();
+	
+	public void Main() {
+		setPreferredSize( new Dimension(( int)( WIDTH * scale), ( int)( HEIGHT * scale)));
+    	frame = new JFrame();
+    	game = new Game();
+	}
+	
     public void start() {
-
+    	running = true;
+    	thread = new Thread (this, "loopThread");
+    	thread.start();
     }
 
     public void stop() {
-
+    	try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+    	
     }
 
     public void run() {
-
+    	long lastTimeInNanoSeconds = System.nanoTime();
+    	long timer = System.currentTimeMillis();
+    	double nanoSecondsPerUpdate = 1000000000.0 / 60.0;
+    	double updatesToPerform = 0.0;
+    	int frames = 0;
+    	int updates = 0;
+    	requestFocus();
+    	while(running) {
+    		long currentTimeInNanoSeconds = System.nanoTime();
+    		updatesToPerform += ( currentTimeInNanoSeconds-lastTimeInNanoSeconds) / nanoSecondsPerUpdate;
+    		if( updatesToPerform >=1) {
+    			update();
+    			updates++;
+    			updatesToPerform--;	
+    		}
+    		lastTimeInNanoSeconds = currentTimeInNanoSeconds;
+    		
+    		render();
+    		frames++;
+    		
+    		if(System.currentTimeMillis() - timer > 1000) {
+    			frame.setTitle("2048 " + updates + " updates, " + frames + " frames" );
+    			updates = 0;
+    			frames = 0;
+    			timer += 1000;
+    		}
+    	}
     }
 
     public void update() {
-
+    	game.update();
+    	
     }
 
     public void render() {
-
+    	BufferStrategy bs = getBufferStrategy();
+    	if( bs == null) {
+    		createBufferStrategy(3);
+    		return;
+    	}
+    	game.render();
+    	
+    	Graphics2D g = (Graphics2D) bs.getDrawGraphics();
+    	g.drawImage( image, 0, 0, ( int)( WIDTH * scale), ( int)( HEIGHT * scale), null);
+    	game.renderText(g);
+    	g.dispose();
+    	bs.show();
+    	
     }
 
     public static void main(String[] args) {
+    	Main m = new Main();
+    	m.frame.setResizable(false);
+    	m.frame.setTitle("2048");
+    	m.frame.add(m);
+    	m.frame.pack();
+    	m.frame.setVisible(true);
+    	m.frame.setLocationRelativeTo(null);
+    	m.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	m.frame.setAlwaysOnTop(true);
+    	m.start();
     }
+ 
 }
