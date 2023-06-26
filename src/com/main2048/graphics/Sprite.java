cae35a8d619a2833f7e58a924ed9b924c1305786
dungameo <dung,hm215543@sis.hust.edu.vn>
2023-06-27 01:39:59 +0700

    chep lai code class GameObject

--- src/com/main2048/graphics/Sprite.java
+++ src/com/main2048/graphics/Sprite.java
@@ -1,7 +1,9 @@
 package com.main2048.graphics;
 
 public class Sprite {
+    public int width=100,height=100;
     public Sprite(int i, int i1, int i2) {
+
     }
     //code tự động gen, cứ sửa tự nhiên
 }
